7a721a58c7dd52f5845eebd118f547ac3acc5a3e
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-12-26 21:40:59 +0530

    Add: Adding a separate layout for mobile version

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,48 @@
+// // import type { NextConfig } from "next";
+
+// // const nextConfig: NextConfig = {
+// //   images: {
+// //     domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+// //     unoptimized: true, // ✅ disable image optimization
+// //   },
+// //   output: "export",
+// // };
+
+// // export default nextConfig;
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+//       },
+//     ],
+//     formats: ["image/avif", "image/webp"],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
-    unoptimized: true, // ✅ disable image optimization
-  },
   output: "export",
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+    unoptimized: true, // keep this ONLY because of static export
+  },
 };
 
 export default nextConfig;
