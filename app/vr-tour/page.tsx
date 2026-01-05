7f6e811905ede827854e7830e18531a6e557644d
PrinceJoshwa <sprincejoshwa85@gmail.com>
2026-01-05 14:22:54 +0530

    Add: Vr Folder

--- app/vr-tour/page.tsx
+++ app/vr-tour/page.tsx
@@ -5,32 +5,32 @@
 // // }
 // "use client"
 
-// import { useEffect } from "react"
-
-// export default function VRTourPage() {
-//   useEffect(() => {
-//     window.location.href =
-//       "http://palmaltezze.s3-website-us-east-1.amazonaws.com/"
-//   }, [])
-
-//   return (
-//     <div className="min-h-screen flex items-center justify-center text-white">
-//       Redirecting to VR Tour…
-//     </div>
-//   )
-// }
-
-"use client"
+import { useEffect } from "react"
 
 export default function VRTourPage() {
+  useEffect(() => {
+    window.location.href =
+      "http://palmaltezze.s3-website-us-east-1.amazonaws.com/"
+  }, [])
+
   return (
-    <div className="w-full h-screen bg-black">
-      <iframe
-        src="https://palmaltezze.s3-website-us-east-1.amazonaws.com/"
-        title="Palm Altezze VR Tour"
-        className="w-full h-full border-0"
-        allow="fullscreen; autoplay; xr-spatial-tracking"
-      />
+    <div className="min-h-screen flex items-center justify-center text-white">
+      Redirecting to VR Tour…
     </div>
   )
 }
+
+// "use client"
+
+// export default function VRTourPage() {
+//   return (
+//     <div className="w-full h-screen bg-black">
+//       <iframe
+//         src="https://palmaltezze.s3-website-us-east-1.amazonaws.com/"
+//         title="Palm Altezze VR Tour"
+//         className="w-full h-full border-0"
+//         allow="fullscreen; autoplay; xr-spatial-tracking"
+//       />
+//     </div>
+//   )
+// }
